14d6c7dbdcf51cb3ea81dff9ae950ce03e040ea8
Felipe Velasquez <felipe.velasquezc@gmail.com>
2017-08-09 01:09:57 -0400

    Start

--- TeleView_Linux.sh
+++ TeleView_Linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Cerraremos Google Chrome
 
-#pkill chromium-browser
-nohup node bin/www > teleview.log & 
-chromium-browser --args --disable-web-security http://localhost:3000 --start-fullscreen 
+pm2 kill && 
+pm2 start /opt/node/TeleView/bin/www --name=teleview && 
+chromium-browser --args http://localhost:3000 --start-fullscreen 
 
 
