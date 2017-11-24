bdd9e15d65d19842218b35a7bb63de7bd6e9206a
Felipe Velasquez <felipe.velasquez@globant.com>
2017-11-23 22:05:06 -0300

    File

--- public/javascripts/mainRemoto.js
+++ public/javascripts/mainRemoto.js
@@ -192,7 +192,7 @@ $(function() {
         var isAdvancedUpload = function() {
 
             var div = document.createElement('div');
-            return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+            return true; //((('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window) || ;
         }();
 
 
